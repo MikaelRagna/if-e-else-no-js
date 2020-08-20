f5546502a920444e14155e903a22ea58d16e1de7
MikaelRagna <mikael.previtera@gmail.com>
2020-08-20 14:33:57 -0300

    js

---
+++ ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`A velocidade da moto é ${vel}`)
+    if (vel>60){
+        console.log('Você ultrapassou a velocidade maxima e foi MULTADO')
+    }
+console.log('dirja sempre usando o capacete')
